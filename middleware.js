991e936d5fb20fe8bc2b5ce17c9bd2e410db35d2
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-04-10 18:09:15 +0100

    auth redirect middleware

---
+++ middleware.js
@@ -0,0 +1,19 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/dist/server/api-utils";
+import { NextResponse } from "next/server";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request) {
+  const { isAuthenticated } = getKindeServerSession();
+  if (!(await isAuthenticated())) {
+    // redirect('/api/auth/login')
+    return NextResponse.redirect(
+      new URL("/api/auth/login?post_login_redirect_url=/", request.url)
+    );
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/details/:path*",
+};
