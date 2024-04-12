0ed02d80a86f43607b22fc3adf1bf16a76f505aa
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-04-12 21:56:51 +0100

    disabled email

--- app/api/auth/[kindeAuth]/sendEmail/route.js
+++ app/api/auth/[kindeAuth]/sendEmail/route.js
@@ -1,23 +1,23 @@
-import EmailTemplate from "@/emails";
-import { NextResponse } from "next/server";
-import { Resend } from "resend";
-import { Email } from "./email";
+// import EmailTemplate from "@/emails";
+// import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import { Email } from "./email";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-export async function POST(req) {
-  const response = await req.json();
-  console.log(response);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+// export async function POST(req) {
+//   const response = await req.json();
+//   console.log(response);
 
-  try {
-    const data = await resend.emails.send({
-      from: "onboarding@resend.dev",
-      to: "chaun.online@gmail.com",
-      subject: "Appointment Booking Confirmation",
-      text: "it works!",
-    });
+//   try {
+//     const data = await resend.emails.send({
+//       from: "onboarding@resend.dev",
+//       to: "chaun.online@gmail.com",
+//       subject: "Appointment Booking Confirmation",
+//       text: "it works!",
+//     });
 
-    return NextResponse.json({ data });
-  } catch (error) {
-    return NextResponse.json({ error });
-  }
-}
+//     return NextResponse.json({ data });
+//   } catch (error) {
+//     return NextResponse.json({ error });
+//   }
+// }
