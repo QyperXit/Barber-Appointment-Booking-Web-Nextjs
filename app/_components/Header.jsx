b640be985a3c5f0c3210dd122d803ec8e5bcf5f4
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-04-13 00:23:05 +0100

    logo

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -40,7 +40,13 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between p-4 shadow-sm">
       <div className="flex items-center gap-10">
-        <Image src="/logo.svg" alt="logo" width={90} height={80} />
+        {/* <Image src="/barber.png" alt="logo" width={90} height={80} /> */}
+        <i className=" text-5xl font-bold  text-amber-400 text-shadow-xl relative">
+          G
+          <span className="absolute text-gray-500  inset-7 after:content-[hello] text-[10px] text-shadow-custom">
+            Barbers
+          </span>
+        </i>
         <ul className=" md:flex gap-8 hidden">
           {Menu.map((item, index) => (
             <Link href={item.path}>
--- app/page.js
+++ app/page.js
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <div>
       <Hero />
-      <CatergorySearch />
+      {/* <CatergorySearch /> */}
       <DoctorList doctorList={doctorList} />
     </div>
   );
