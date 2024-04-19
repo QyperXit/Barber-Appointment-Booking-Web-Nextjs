509cc33aee3af4333945f847d893894e91a0faa0
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-04-19 01:50:18 +0100

    clean up

--- app/_components/BarberList.jsx
+++ app/_components/BarberList.jsx
@@ -32,7 +32,7 @@ const BarberList = ({ doctor }) => {
     //   <div className="relative p-6 md:p-16">
     //     <div className="relative z-10 lg:grid lg:grid-cols-12 lg:gap-16 lg:items-center">
     //       <div className="mb-10 lg:mb-0 lg:col-span-6 lg:col-start-8 lg:order-2">
-    //         <h2 className="text-2xl text-gray-800 font-bold sm:text-3xl dark:text-gray-200">
+    //         <h2 className="text-2xl font-bold text-gray-800 sm:text-3xl dark:text-gray-200">
     //           Tailored cuts, personalized for your style.
     //         </h2>
 
@@ -42,7 +42,7 @@ const BarberList = ({ doctor }) => {
     //           role="tablist"
     //         >
     //           <div
-    //             className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 active"
+    //             className="p-4 hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700 active"
     //             id="tabs-with-card-item-1"
     //             data-hs-tab="#tabs-with-card-1"
     //             aria-controls="tabs-with-card-1"
@@ -55,12 +55,12 @@ const BarberList = ({ doctor }) => {
     //                   width={24}
     //                   height={24}
     //                   alt="scissors"
-    //                   className="flex-shrink-0 mt-2 size-6 md:size-7 hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
+    //                   className="flex-shrink-0 mt-2 text-gray-800 size-6 md:size-7 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
     //                 />
     //               )}
 
     //               <span className="grow ms-6">
-    //                 <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-400 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
+    //                 <span className="block text-lg font-semibold text-gray-400 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
     //                   Coming Soon ....
     //                 </span>
     //                 <div className="block mt-1 text-gray-800 dark:hs-tab-active:text-gray-200 dark:text-gray-200">
@@ -72,7 +72,7 @@ const BarberList = ({ doctor }) => {
 
     //           <div
     //             type="button"
-    //             className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700"
+    //             className="p-4 hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700"
     //             id="tabs-with-card-item-2"
     //             data-hs-tab="#tabs-with-card-2"
     //             aria-controls="tabs-with-card-2"
@@ -80,7 +80,7 @@ const BarberList = ({ doctor }) => {
     //           >
     //             <span className="flex">
     //               {/* <svg
-    //                 className="flex-shrink-0 mt-2 size-6 md:size-7 hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
+    //                 className="flex-shrink-0 mt-2 text-gray-800 size-6 md:size-7 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
     //                 xmlns="http://www.w3.org/2000/svg"
     //                 width="24"
     //                 height="24"
@@ -97,14 +97,14 @@ const BarberList = ({ doctor }) => {
     //                   width={24}
     //                   height={24}
     //                   alt="scissors"
-    //                   className="flex-shrink-0 mt-2 size-6 md:size-7 hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
+    //                   className="flex-shrink-0 mt-2 text-gray-800 size-6 md:size-7 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
     //                 />
     //               )}
     //               {/* <path d="m12 14 4-4" />
     //                 <path d="M3.34 19a10 10 0 1 1 17.32 0" />
     //               </svg> */}
     //               <span className="grow ms-6">
-    //                 <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
+    //                 <span className="block text-lg font-semibold text-gray-800 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
     //                   Micheal
     //                 </span>
     //                 <Link
@@ -119,7 +119,7 @@ const BarberList = ({ doctor }) => {
 
     //           <div
     //             type="button"
-    //             className="hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 p-4 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700"
+    //             className="p-4 hs-tab-active:bg-white hs-tab-active:shadow-md hs-tab-active:hover:border-transparent text-start hover:bg-gray-200 md:p-5 rounded-xl dark:hs-tab-active:bg-slate-900 dark:hover:bg-gray-700"
     //             id="tabs-with-card-item-3"
     //             data-hs-tab="#tabs-with-card-3"
     //             aria-controls="tabs-with-card-3"
@@ -132,11 +132,11 @@ const BarberList = ({ doctor }) => {
     //                   width={24}
     //                   height={24}
     //                   alt="scissors"
-    //                   className="flex-shrink-0 mt-2 size-6 md:size-7 hs-tab-active:text-blue-600 text-gray-800 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
+    //                   className="flex-shrink-0 mt-2 text-gray-800 size-6 md:size-7 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200"
     //                 />
     //               )}
     //               <span className="grow ms-6">
-    //                 <span className="block text-lg font-semibold hs-tab-active:text-blue-600 text-gray-400 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
+    //                 <span className="block text-lg font-semibold text-gray-400 hs-tab-active:text-blue-600 dark:hs-tab-active:text-blue-500 dark:text-gray-200">
     //                   Coming Soon...
     //                 </span>
     //                 <div className="block mt-1 text-gray-800 dark:hs-tab-active:text-gray-200 dark:text-gray-200">
@@ -182,7 +182,7 @@ const BarberList = ({ doctor }) => {
     //             </div>
     //           </div>
 
-    //           <div className="hidden absolute top-0 end-0 translate-x-20 md:block lg:translate-x-20">
+    //           <div className="absolute top-0 hidden translate-x-20 end-0 md:block lg:translate-x-20">
     //             {/* <svg
     //               className="w-16 h-auto text-amber-500"
     //               width="121"
@@ -220,69 +220,69 @@ const BarberList = ({ doctor }) => {
     //     </div>
     //   </div>
     // </div>
-    <div className="md:max-w-4xl mx-auto mt-20 mb-10">
-      <div className="grid grid-cols-2 sm:grid-cols-4  gap-2">
+    <div className="mx-auto mt-20 mb-10 md:max-w-4xl">
+      <div className="grid grid-cols-2 gap-2 sm:grid-cols-4">
         <div className="space-y-2">
           <img
-            className="w-full sm:size-40 lg:size-60 object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1593702275687-f8b402bf1fb5?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="/1barber.webp"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1557349800-5b9b168f3f53?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fGJhcmJlciUyMGNsaXBzfGVufDB8fDB8fHww"
             alt="Image Description"
           />
         </div>
         <div className="space-y-2">
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1514336937476-a5b961020a5c?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Image Description"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="/3barber.webp"
             alt="Image Description"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="/2barber.jpg"
             alt="Image Description"
           />
         </div>
         <div className="space-y-2">
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1493256338651-d82f7acb2b38?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1599351431202-1e0f0137899a?q=80&w=1288&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Image Description"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1523532931495-f51ebf4bc3a2?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTF8fGJhcmJlciUyMGhhaXIlMjBjdXR8ZW58MHx8MHx8fDA%3D"
             alt="Image Description"
           />
         </div>
         <div className="space-y-2">
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1657105052497-f996284ffff8?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjl8fGJhcmJlciUyMGNsaXBzfGVufDB8fDB8fHww"
             alt="Image Description"
           />
           <img
-            className="w-full sm:size-40 lg:size-60  object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://images.unsplash.com/photo-1536520002442-39764a41e987?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8YmFyYmVyJTIwY2xpcHN8ZW58MHx8MHx8fDA%3D"
             alt="Image Description"
           />
           <img
-            className="w-full sm:size-40  lg:size-60 object-cover border-4"
+            className="object-cover w-full border-4 sm:size-40 lg:size-60"
             src="https://plus.unsplash.com/premium_photo-1661382022096-d652c06cf1be?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Image Description"
           />
--- app/_components/DoctorList.jsx
+++ app/_components/DoctorList.jsx
@@ -5,9 +5,9 @@ import React from "react";
 
 const DoctorList = ({ doctorList, heading = "Popular Doctors" }) => {
   return (
-    // <div className=" mb-10 mt-10 px-6">
-    //   <h2 className=" font-bold text-xl">{heading}</h2>
-    //   <div className=" grid grid-cols-2 gap-7 mt-4  md:grid-cols-3 lg:grid-cols-4">
+    // <div className="px-6 mt-10 mb-10 ">
+    //   <h2 className="text-xl font-bold ">{heading}</h2>
+    //   <div className="grid grid-cols-2 mt-4  gap-7 md:grid-cols-3 lg:grid-cols-4">
     //     {doctorList.length > 0
     //       ? doctorList.map((doctor, index) => (
     //           <div
@@ -21,18 +21,18 @@ const DoctorList = ({ doctorList, heading = "Popular Doctors" }) => {
     //               alt="doctor img"
     //               className="h-[200px] w-full object-cover object-center  rounded-lg"
     //             />
-    //             <div className=" mt-3 items-baseline flex  gap-2 flex-col">
+    //             <div className="flex flex-col items-baseline gap-2 mt-3 ">
     //               <h2 className="text-[10px] font-bold bg-blue-100 p-1 rounded-full px-2 text-primary">
     //                 {doctor.attributes?.catergories?.data[0]?.attributes?.Name}
     //               </h2>
-    //               <h2 className=" mt-2 font-bold">{doctor.attributes.Name}</h2>
-    //               <h2 className=" text-primary text-sm">
+    //               <h2 className="mt-2 font-bold ">{doctor.attributes.Name}</h2>
+    //               <h2 className="text-sm  text-primary">
     //                 {doctor.attributes?.Years_of_experience}
     //               </h2>
     //               <h2 className="text-sm text-gray-500">
     //                 {doctor.attributes?.Address}
     //               </h2>
-    //               <Link href={"/details/" + doctor?.id} className=" w-full">
+    //               <Link href={"/details/" + doctor?.id} className="w-full ">
     //                 <h2 className=" p-2 px-3 border-[1px] border-primary  text-primary rounded-full w-full text-center text-[11px] mt-2  cursor-pointer hover:bg-primary  hover:text-white ransition-all ease-in-out">
     //                   Book Now
     //                 </h2>
@@ -51,9 +51,9 @@ const DoctorList = ({ doctorList, heading = "Popular Doctors" }) => {
     //   </div>
     // </div>
 
-    <div className=" mb-10 mt-10 px-6">
-      <h2 className=" font-bold text-xl">{heading}</h2>
-      <div className=" grid grid-cols-2 gap-7 mt-4  md:grid-cols-3 lg:grid-cols-4">
+    <div className="px-6 mt-10 mb-10 ">
+      <h2 className="text-xl font-bold ">{heading}</h2>
+      <div className="grid grid-cols-2 mt-4  gap-7 md:grid-cols-3 lg:grid-cols-4">
         {doctorList.length > 0
           ? doctorList.map((doctor, index) => (
               <div
@@ -67,18 +67,18 @@ const DoctorList = ({ doctorList, heading = "Popular Doctors" }) => {
                   alt="doctor img"
                   className="h-[200px] w-full object-cover object-center  rounded-lg"
                 />
-                <div className=" mt-3 items-baseline flex  gap-2 flex-col">
+                <div className="flex flex-col items-baseline gap-2 mt-3 ">
                   <h2 className="text-[10px] font-bold bg-blue-100 p-1 rounded-full px-2 text-primary">
                     {doctor.attributes?.catergories?.data[0]?.attributes?.Name}
                   </h2>
-                  <h2 className=" mt-2 font-bold">{doctor.attributes.Name}</h2>
-                  <h2 className=" text-primary text-sm">
+                  <h2 className="mt-2 font-bold ">{doctor.attributes.Name}</h2>
+                  <h2 className="text-sm  text-primary">
                     {doctor.attributes?.Years_of_experience}
                   </h2>
                   <h2 className="text-sm text-gray-500">
                     {doctor.attributes?.Address}
                   </h2>
-                  <Link href={"/details/" + doctor?.id} className=" w-full">
+                  <Link href={"/details/" + doctor?.id} className="w-full ">
                     <h2 className=" p-2 px-3 border-[1px] border-primary  text-primary rounded-full w-full text-center text-[11px] mt-2  cursor-pointer hover:bg-primary  hover:text-white ransition-all ease-in-out">
                       Book Now
                     </h2>
