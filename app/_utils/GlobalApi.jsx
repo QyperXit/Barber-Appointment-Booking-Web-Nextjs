c53bc8894626b99608ec812257abd0c9ab2f0b1b
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-04-20 15:45:41 +0100

    api again

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,7 +3,7 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "https://appointment-booking-backend-s6js.onrender.com/api/", // Note the correct spelling of baseURL
+  baseURL: "https://appointment-booking-backend-s6js.onrender.com/api", // Note the correct spelling of baseURL
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
