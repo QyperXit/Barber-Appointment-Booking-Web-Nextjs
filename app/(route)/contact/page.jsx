0d85f42fe8977c2e135a20d7694bfd6ab4ec37fb
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-05-02 00:50:00 +0100

    contact Gmap resize

--- app/(route)/contact/page.jsx
+++ app/(route)/contact/page.jsx
@@ -7,18 +7,18 @@ const Contact = () => {
     <section className="flex flex-col items-center mt-12 text-white gap-y-8">
       <h1 className="text-5xl font-bold ">GET IN TOUCH</h1>
 
-      <div className="flex items-center justify-center gap-20 m-12 ">
+      <div className="flex flex-col items-center justify-center gap-20 m-12 md:flex-row ">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d559.8514543602671!2d-1.85289099856035!3d52.45258012468219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bbc005471653%3A0xa6301f641b71c3d6!2s%22G%22%20Barbers!5e0!3m2!1sen!2suk!4v1714605833394!5m2!1sen!2suk"
-          width="600"
+          // width="600"
           height="450"
-          className="border-[8px] border-white"
+          className="border-[8px] border-white object-contain md:w-[500px] sm:w-[300px] lg:w-[600px] "
           // style="border:0;"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
         ></iframe>
-        <div className="flex flex-col justify-center gap-14">
+        <div className="flex justify-center md:flex-col gap-14">
           <div>
             <MapIcon className="mx-auto scale-150 " />
             <h1 className="mt-3 text-2xl font-bold text-center ">Address</h1>
