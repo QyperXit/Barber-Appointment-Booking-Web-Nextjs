5a7c2930152ee2be0991c23a0cd0c6a1a3e9d6bf
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-05-13 22:02:38 +0100

    search fix deploy

--- app/(route)/search/layout.js
+++ app/(route)/search/layout.js
@@ -1,16 +1,16 @@
-import React from "react";
-import CatergoryList from "./_components/CatergoryList";
+// import React from "react";
+// import CatergoryList from "./_components/CatergoryList";
 
-const layout = ({ children }) => {
-  return (
-    <div className=" grid grid-cols-4">
-      <div className="hidden md:block">
-        <CatergoryList />
-      </div>
+// const layout = ({ children }) => {
+//   return (
+//     <div className="grid grid-cols-4 ">
+//       <div className="hidden md:block">
+//         <CatergoryList />
+//       </div>
 
-      <div className=" ml-5 mt-[-7px] col-span-4 md:col-span-3">{children}</div>
-    </div>
-  );
-};
+//       <div className=" ml-5 mt-[-7px] col-span-4 md:col-span-3">{children}</div>
+//     </div>
+//   );
+// };
 
-export default layout;
+// export default layout;
