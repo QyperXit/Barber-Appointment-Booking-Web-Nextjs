eba90f9e0d1f75a653448378d114dca361dbe953
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-09-06 00:12:09 +0100

    email-confirmations

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -23,14 +23,14 @@ export async function POST(req) {
     if (!response?.data?.data?.id) {
       additionalEmailData = await resend.emails.send({
         from: "gbarbers@shotsbyvidz.com",
-        to: ["chaun.online@gmail.com"],
+        to: ["denisjazzy@yahoo.com"],
         subject: "New Appointment Booked",
         react: BarberTemplate({ response }),
       });
     } else if (response?.data?.data?.id) {
       additionalEmailData = await resend.emails.send({
         from: "gbarbers@shotsbyvidz.com",
-        to: ["chaun.online@gmail.com"],
+        to: ["denisjazzy@yahoo.com"],
         subject: "Appointment Cancelled",
         react: CancellationTemplate({ response }),
       });
