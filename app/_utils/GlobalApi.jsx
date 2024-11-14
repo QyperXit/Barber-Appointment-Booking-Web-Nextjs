069ea62d4b3c254b88c4e52df0b61402c532dafa
QyperXit <67711154+QyperXit@users.noreply.github.com>
2024-11-14 14:17:33 +0000

    fixes and patches

--- app/_utils/GlobalApi.jsx
+++ app/_utils/GlobalApi.jsx
@@ -3,52 +3,71 @@ const { default: axios } = require("axios");
 const API_KEY = process.env.NEXT_PUBLIC_STRAPI_API_KEY;
 
 const axiosClient = axios.create({
-  baseURL: "https://appointment-booking-backend-s6js.onrender.com/api/", // Note the correct spelling of baseURL
+  baseURL: "https://appointment-booking-backend-s6js.onrender.com/api/",
   headers: {
     Authorization: `Bearer ${API_KEY}`,
   },
 });
 
 const getCatergory = () => axiosClient.get("/catergories?populate=*");
-const getDoctorList = () => axiosClient.get("/doctors?populate=*");
+const getBarberList = () => axiosClient.get("/doctors?populate=*");
 const getDoctorByCategory = (category) =>
-  axiosClient.get(
-    `/doctors?filters[catergories][Name][$in]=${category}&populate=*`
-  );
+    axiosClient.get(
+        `/doctors?filters[catergories][Name][$in]=${category}&populate=*`
+    );
 
 const bookApointment = async (data) => {
   try {
     const response = await axiosClient.post("/appointments", data);
-    // Handle successful booking (e.g., display a confirmation message)
-    return response.data; // Optional: return the response data if needed
+
+    return response.data;
   } catch (error) {
     console.error("Error booking appointment:", error);
-    // Handle errors (e.g., display an error message to the user)
   }
 };
 
-// console.log(bookApointment());
-
-const doctorId = 3; // Replace with the actual doctor ID
 const getDoctorById = async (id) => {
   try {
     const response = await axiosClient.get(`/doctors/${id}?populate=*`);
+
     return response.data; // Return doctor data
   } catch (error) {
     console.error("Error fetching doctor:", error);
-    // Handle error (e.g., display an error message to the user)
+  }
+};
+
+// Function to fetch appointment details by ID
+const getAppointmentById = async (id) => {
+  try {
+    const response = await axiosClient.get(`/appointments/${id}?populate=*`);
+    return response.data; // Return the appointment data including the ID
+  } catch (error) {
+    console.error("Error fetching appointment:", error);
+    throw error; // Re-throw the error to handle it elsewhere
   }
 };
 
 // const sendEmail = (data) => axios.post("/api/sendEmail", data);
-// const sendEmail = async (data) => {
-//   try {
-//     const response = await axios.post("/api/sendEmail", data);
-//     console.log("Email sent successfully:", response.data); // Handle success
-//   } catch (error) {
-//     console.error("Error sending email:", error); // Handle errors
-//   }
-// };
+const sendEmail = async (data) => {
+  try {
+    const response = await axios.post("/api/sendEmail", data);
+    console.log("Email sent successfully:", response.data);
+    return response.data;
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error; // Re-throw the error so it can be caught and handled by the caller
+  }
+};
+
+const getAppointments = async () => {
+  try {
+    const response = await axiosClient.get("/appointments");
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching appointments:", error);
+  }
+};
 
 const getUserBookingList = (userEmail) => {
   const params = new URLSearchParams({
@@ -60,17 +79,34 @@ const getUserBookingList = (userEmail) => {
   return axiosClient.get(`/appointments?${params}`);
 };
 
+const updateAppointmentStatus = async (id, status) => {
+  try {
+    const response = await axiosClient.put(`/appointments/${id}`, {
+      // data: { status: status },
+      data: { status },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Error updating appointment status:", error);
+    throw error;
+  }
+};
+
 const DeleteBooking = (id) => axiosClient.delete(`/appointments/${id}`);
 const GetIcons = (id) => axiosClient.get(`/icons/${id}/?populate=*`);
 
 export default {
   getCatergory,
-  getDoctorList,
+  getBarberList,
   getDoctorByCategory,
   getDoctorById,
   bookApointment,
-  // sendEmail,
+  sendEmail,
   GetIcons,
   getUserBookingList,
   DeleteBooking,
+  getAppointments,
+  updateAppointmentStatus,
+  getAppointmentById,
+  // checkExistingBookings,
 };
